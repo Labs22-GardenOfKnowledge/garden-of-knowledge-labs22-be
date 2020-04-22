0e9be5a04e7813a76dfd4644ba4eb41a423ab77f
Richard Machado <richardmachado@MacBook-Pro.local>
2020-04-22 10:03:04 -0700

    creating familes

--- api/models/family.model.js
+++ api/models/family.model.js
@@ -2,9 +2,11 @@ const db = require('../../database/db-config');
 
 
 
-const findAllFamilies = () => {
+const findAllFamilies = user_id => {
   return db('family')
-    .select(['user_id','mother_name', 'father_name'])
+    .join('student','students.family_id')
+    .where('family_id', '=', user_id  )
+    .select(['user_id','mother_name', 'father_name','block_code','road','building','flat','primary_telephone','secondary_telephone'])
    
 };
 
